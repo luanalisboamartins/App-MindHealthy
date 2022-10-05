8e701064ea64f779596bbece0efaf2c7baf2fd54
Juliano <silva.juliano8130@gmail.com>
2022-10-05 09:54:41 -0300

    Listando Postagem

--- app/src/main/java/com/example/app_mindhealthy/model/PostagemModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.app_mindhealthy.model
-
-    data class PostagemModel (
-        val titulo: String,
-        val descricao: String,
-        val imagem: String,
-        val tema: String
-        ){
-}
